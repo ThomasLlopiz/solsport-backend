11cdc22e8afafc05a97cad7698b1cd74a8820644
ThomasLlopiz <toomillopiz@gmail.com>
2024-08-05 11:40:47 -0300

    local

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
 
-router.post('/backend/login', usuariosController.login);
-router.get('/backend/', usuariosController.getAll);
-router.get('/backend/:id', usuariosController.getById);
-router.post('/backend/', usuariosController.create);
-router.put('/backend/:id', usuariosController.update);
-router.delete('/backend/:id', usuariosController.delete);
+router.post('/login', usuariosController.login);
+router.get('/', usuariosController.getAll);
+router.get('/:id', usuariosController.getById);
+router.post('/', usuariosController.create);
+router.put('/:id', usuariosController.update);
+router.delete('/:id', usuariosController.delete);
 
 module.exports = router;
