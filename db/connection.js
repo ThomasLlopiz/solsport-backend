58f9d7c9b67184169f115dd58c1e1435f16c380d
ThomasLlopiz <toomillopiz@gmail.com>
2024-08-05 11:11:56 -0300

    PUERTO

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require("mysql");
+// require('dotenv').config();
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: 'capyccom_santiago',
-  password: '43997224Santi',
-  database: 'capyccom_solsport',
-  port: 3306
+  user: "capyccom_santiago",
+  password: "43997224Santi",
+  database: "capyccom_solsport",
+  port: process.env.PORT || 3306,
 });
 
-connection.connect((err) => {
+db.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error("Error connecting to the database:", err);
     return;
   }
-  console.log('Connected to the database.');
+  console.log("Connected to the database.");
 });
 
-module.exports = connection;
+module.exports = db;
