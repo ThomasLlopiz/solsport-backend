54e3c8fdfb394eaa77fbaf27875beb2175ee3e81
ThomasLlopiz <toomillopiz@gmail.com>
2024-08-12 08:35:58 -0300

    a

--- app.js
+++ app.js
@@ -35,12 +35,6 @@ app.use("/pedidos", pedidosRoutes);
 app.use("/articulos", articulosRoutes);
 app.use("/etapas", etapasRoutes);
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
