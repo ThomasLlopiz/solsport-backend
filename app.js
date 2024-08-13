d7b858682f8ddbb82a7a5b3779646ea470c8c628
ThomasLlopiz <toomillopiz@gmail.com>
2024-08-13 08:54:34 -0300

    a

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const etapasRoutes = require("./routes/etapasRoutes");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.URLFRONTEND || "http://localhost:5173",
     credentials: true,
   })
 );
