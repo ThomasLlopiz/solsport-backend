7ba3a8bb9da1ac07ee449badcc430c1e973bec25
ThomasLlopiz <toomillopiz@gmail.com>
2024-11-27 11:49:20 -0300

    cambios

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
 require('dotenv').config();
 const db = mysql.createConnection({
+  
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_DATABASE
 });
-//mysql://root:NyHSfAclCKywCDkzBOMsFXktXJONTDdL@roundhouse.proxy.rlwy.net:13122/railway
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to the database:", err);
