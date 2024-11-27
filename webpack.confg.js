6f7ac326bf94f0698267ef35ee9f687f1515c498
ThomasLlopiz <toomillopiz@gmail.com>
2024-11-27 10:47:44 -0300

    para probar otra vez

--- webpack.confg.js
+++ webpack.confg.js
@@ -2,19 +2,19 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
+    mode: 'production', // O 'development' si es en desarrollo
+    entry: './src/index.js', // Aquí especificamos la entrada desde 'src/index.js'
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    target: 'node',
-    externals: [nodeExternals()],
+    target: 'node', // Asegúrate de que sea para un entorno Node.js
+    externals: [nodeExternals()], // Excluye dependencias de node_modules
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
+                test: /\.js$/, // Aplica babel-loader a archivos .js
+                exclude: /node_modules/, // Excluye node_modules
                 use: {
                     loader: 'babel-loader'
                 }
